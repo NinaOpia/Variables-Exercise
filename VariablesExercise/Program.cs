faea9943a126d9b5d714f8e4629377b272fee23a
Nina <boldurfaith@gmail.com>
2023-03-06 02:24:19 -0800

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName; //Declaration Syntax
+
+            dogName = "Rex"; //Initialization
+
+            int dogAge = 5; //Declaration and Initialization Syntax
+
+            char firstInitial = 'R';
+
+            bool isHungry = true;
+
+            double dogWeight = 54.3;
+
+            decimal dogHeight = 14.7m;
+
+            Console.WriteLine ($"My dog's name is {dogName} and he is {dogAge} years old.");
+
+            Console.WriteLine ($"It is {isHungry} that he is always hungry. He is {dogWeight} lbs and is {dogHeight} inches tall.");
+
         }
     }
 }
